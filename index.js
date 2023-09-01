4129f28b7466f660761d1630f769bd23aab37f8b
rhaalsch <143081439+rhaalsch@users.noreply.github.com>
2023-09-01 15:19:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+function generatePassword() {
+  let firstPassword = document.getElementById("firstPassword");
+  let secondPassword = document.getElementById("secondPassword");
+  let firstSet = "";
+  let secondSet = "";
+  for (let i = 0; i < 15; i++) {
+    firstSet += characters[getRandomIndex()];
+    secondSet += characters[getRandomIndex()];
+  }
+
+  if (firstSet.length < 16 && secondSet.length < 16) {
+    firstPassword.textContent = firstSet;
+    secondPassword.textContent = secondSet;
+  }
+}
+
+function getRandomIndex() {
+  return Math.floor(Math.random() * characters.length);
+}
+
+function renderPassword() {
+  generatePassword();
+}
